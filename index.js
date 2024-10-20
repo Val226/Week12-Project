4a802d8aa8d9adabe308e94ac88ef5a2fe5f62cc
Val226 <valboddy@gmail.com>
2024-10-20 16:29:04 -0600

    Getting started!

---
+++ index.js
@@ -0,0 +1,20 @@
+/*  Instructions:
+Create a CRD application (CRUD without update) using json-server or another API
+Use fetch and async/await to interact with the API
+Use a form to create/post new entities
+Build a way for users to delete entities
+Include a way to get entities from the API and display them
+You do NOT need update, but you can add it if you'd like
+Use Bootstrap and/or CSS to style your project
+
+Needs:
+HTML:
+  - Buttons to:
+    - Create/Post
+    - Delete
+  - Form
+
+  JS:
+    - A CRD  Application - use json-server or another API
+    
+*/
