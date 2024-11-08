6ae2ccb7725e49f2f79839856e365c7f1d3476e8
Val226 <valboddy@gmail.com>
2024-11-08 13:07:55 -0700

    Lot's of changes!

--- books.js
+++ books.js
@@ -1,20 +1,74 @@
+console.log("Starting book app....");
 
-const myButton = document.getElementById('submitForm');
+
+const myButton = document.getElementById('add');
+// console.log("My button:", myButton);
+
+const API_ENDPOINT = "http://localhost:3000/books"
 
 myButton.addEventListener('click', (event) => {
-    event.preventDefault();
+//console.log("Clicking on my button...");
+
+//Prevents the default behavior of the element ie. a submit button would otherwise refresh the page.
+event.preventDefault();
+
+
+let books = [];
+
+//GET Request
+async function getData(bookTitleVariable) {
+function addBooks() {
+
+console.log("Getting data...");
+
+console.log("Books array before api call:", books);
+
+  try {
+    const response = await fetch(API_ENDPOINT,{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          bookTitle: bookTitleVariable,
+          authorName: "Your Author Here..."
+        }
+      )
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log("JSON FROM API:",json);
+books =json;
+
+console.log("These are my books!",books);
+
+
+  } catch (error) {
+    console.error("Oh No!!! AN eRROR!!!!",error.message);
+  }
+}
+
+
+getData("My Book") //Calling on the getData method to fetch books via the db.json API
+
 
     // I need variables to hold the values of the form
-    let bookTitle = document.getElementById('title').value;
-    let authorName = document.getElementById('author').value;
+    let bookTitle = document.getElementById('bookTitle').value;
+    console.log(`${bookTitle}`)
+    let authorName = document.getElementById('authorName').value;
+    console.log(`${authorName}`)
 
-    //Create a tr node - to append to my form
+
 
     let newTableRow = document.createElement('tr');
 
     let bookTitleNode = document.createElement('td');
     bookTitleNode.innerHTML = title;
-    newTableRow.append(bookTitleNodeNode);
+    newTableRow.append(bookTitleNode);
 
     let authorNameNode = document.createElement('td');
     authorNameNode.innerHTML = authorName;
@@ -24,6 +78,7 @@ myButton.addEventListener('click', (event) => {
 })
 
 // Delete Row
+/*
 function deleteRow(button) {
   console.log("Delete button clicked")
 
@@ -36,3 +91,4 @@ function deleteRow(button) {
     console.log("Row not found, unable to delete")
   }
 }
+  */
